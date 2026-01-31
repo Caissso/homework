16a7f2e1f3bb618a831e0409b266d8b33fff7238
Caissso <murtazzow@gmail.com>
2026-01-31 03:03:25 +0100

    Add files via upload

---
+++ Homework JS-9/main.js
@@ -0,0 +1,11 @@
+let firstName = "Caisso";
+let lastName = "Axma";
+let isStudent = true;
+
+let age = 20;
+let currentYear = 2025;
+let birthYear = currentYear - age;
+
+console.log(`My name is ${firstName} ${lastName}, I am ${age} years old. I am a course student: ${isStudent}.`);
+
+let result = "123456789falsetrue";
